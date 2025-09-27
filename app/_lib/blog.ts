2ad37420200b94835d5856d6af140954bea5c6ca
ToqaUsama136 <toqausama2003@gmail.com>
2025-09-27 18:14:53 +0300

    feat: add blog component

---
+++ app/_lib/blog.ts
@@ -0,0 +1,33 @@
+export type BlogData = {
+  img: string;
+  btn: string;
+  text: string;
+  link: string;
+};
+
+export const Blog: BlogData[] = [
+  {
+    img: '/images/Blog1.png',
+    link: '#',
+    btn: 'Article',
+    text: 'The Future of AI Automation: How It’s Changing Business Operations',
+  },
+  {
+    img: '/images/Blog1.png',
+    link: '#',
+    btn: 'Resources',
+    text: 'The Future of AI Automation: How It’s Changing Business Operations',
+  },
+  {
+    img: '/images/Blog1.png',
+    link: '#',
+    btn: 'Article',
+    text: 'The Future of AI Automation: How It’s Changing Business Operations',
+  },
+  {
+    img: '/images/Blog1.png',
+    link: '#',
+    btn: 'Article',
+    text: 'The Future of AI Automation: How It’s Changing Business Operations',
+  },
+];
